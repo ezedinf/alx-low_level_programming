ac4df3d3f14b37977a0e6c0d599f614cfeda5812
Ezedin Fedilu <ezedinfedilu2@gmail.com>
2024-06-24 22:39:39 +0300

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int figure = 48;
+
+while (figure <= 57)
+{
+putchar(figure);
+figure++;
+}
+putchar('\n');
+
+return (0);
+}
